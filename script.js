1bcf3eed7b2bfbda52db088f258b8e726ac50331
Tianyi Lin <tianyi.lin.tl@gmail.com>
2021-05-08 11:01:32 -0700

    comments and more function

--- script.js
+++ script.js
@@ -1,26 +1,73 @@
 var count = 0;
 //------------------------------------------------------------------------------ <= 80 character limit
 
+//|||||
+//function that starts cooking and calls lemon or sugar
 function startCooking() {
     count++
-    var step_s_ = "";
+    var step_s__p_ = "";
     if (count == 1) {
-        step_s_ = lemon();
+        step_s__p_ = lemon();
     } else if (count == 2) {
-        step_s_ = sugar();
+        step_s__p_ = water();
+    }else if (count == 3) {
+        step_s__p_ = sugar();
+    }else if (count == 4) {
+        step_s__p_ = mix();
+    }else if (count == 5) {
+        step_s__p_ = lemonslice();
+    }else if (count == 6) {
+        step_s__p_ = umbrella();
+    }else if (count == 7) {
+        step_s__p_ = boil();
     }
-    
-    cook(step_s_);
+    cook(step_s__p_);
 }
 
+//|||||
+//function that adds to the step input
 function cook(step) {
     document.body.append(step);
 }
 
+//|||||
+//function that gets you lemon juice
 function lemon() {
     return "squeezing lemons";
 }
 
+//|||||
+//function that gets you lemon juice
+function water() {
+    return "adds in 2 cups of icy crisp water";
+}
+
+//|||||
+//function that adds in sugar
 function sugar() {
-    return " mixing in sugar";
+    return "adding a healthy dose of sugar";
+}
+
+//|||||
+//function that mixes everything up
+function mix() {
+    return "mixing in sugar";
+}
+
+//|||||
+//function that adds lemon aesthetic
+function lemonslice() {
+    return "adding a freshly cut lemon slice";
+}
+
+//|||||
+//function that adds cute umbrella
+function umbrella() {
+    return "adding a cocktail umbrella to top things off!";
+}
+
+//|||||
+//function that adds starts the hotpot
+function boil() {
+    return "whatever the first step is";
 }
